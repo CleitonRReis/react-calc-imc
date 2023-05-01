f99f94a3aa928dd6ff9e26161858463e02128686
Cleiton Reis <cleiton-rodrigues30@live.com>
2023-05-01 18:50:02 -0300

    First commit.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
+import powredImage from './assets/powered.png';
+import leftArrowImage from './assets/leftarrow.png';
+import { useState } from 'react';
+import { levels, calculateIMC, Level } from './helpers/imc';
+import { GridItem } from './components/GridItem';
 
-function App() {
+const App = () => {
+  const [heightField, setHeightField] = useState<number>(0);
+  const [weightField, setWeightField] = useState<number>(0);
+  const [showIMC, setShowIMC] = useState<Level | null>(null);
+
+  const handleCalculateButton = () => {
+    if (heightField && weightField) {
+      setShowIMC(calculateIMC(heightField, weightField));
+      return;
+    }
+
+    alert('Informe todos os campos');
+  }
+
+  const handleBackButton = () => {
+    setShowIMC(null);
+    setHeightField(0);
+    setWeightField(0);
+  }
+
+  const disabledFields = showIMC ? true : false;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className={styles.main}>
+      <header>
+        <div className={styles.headerContainer}>
+          <img src={powredImage} alt="" width={150}/>
+        </div>
       </header>
-    </div>
+      <div className={styles.container}>
+        <div className={styles.leftSide}>
+          <h1>Calcule seu IMC</h1>
+          <p>A OMS (Organização Mundial da Saúde) define o IMC como uma medida padronizada da relação entre o peso e a altura de um indivíduo. Segundo a OMS, o IMC é uma ferramenta útil para avaliar o estado nutricional de indivíduos e populações.</p>
+
+          <input
+            type="number"
+            placeholder="Digite sua altura. Ex: 1.75 (em métros)"
+            value={heightField > 0 ? heightField : ''}
+            onChange={e => setHeightField(parseFloat(e.target.value))}
+            disabled={disabledFields}
+          />
+
+          <input 
+            type="number"
+            placeholder="Digite seu peso. Ex: 65.3 (em kg)"
+            value={weightField > 0 ? weightField : ''}
+            onChange={e => setWeightField(parseFloat(e.target.value))}
+            disabled={disabledFields}
+          />
+
+          <button onClick={handleCalculateButton} disabled={disabledFields}>Calcular</button>
+        </div>
+        <div className={styles.rightSide}>
+            {
+              !showIMC &&
+              <div className={styles.grid}>
+                {
+                  levels.map((item, key) => (
+                    <GridItem key={key} item={item} />
+                  ))
+                }
+              </div>
+            }
+            {
+              showIMC &&
+              <div className={styles.rightBig}>
+                <div className={styles.rightArrow} onClick={handleBackButton}>
+                  <img src={leftArrowImage} alt="" width="25"/>
+                </div>
+                <GridItem item={showIMC} />
+              </div>
+            }
+          </div>
+        </div>
+      </div>
   );
 }
 
